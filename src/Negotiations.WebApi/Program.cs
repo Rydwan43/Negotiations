f1c919690bd89bc7721d76db65a180a764da38fa
Mateusz Radwański <mateuszradwanski@icloud.com>
2022-03-08 19:46:19 +0100

    Integration all layers into presentation layer

--- src/Negotiations.WebApi/Program.cs
+++ src/Negotiations.WebApi/Program.cs
@@ -1,13 +1,26 @@
+using Negotiations.Application;
+using Negotiations.Infrastructure;
+using Negotiations.Infrastructure.DatabaseContext;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(configuration);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<NegotiationsSeeder>();
+
+seeder.Seed();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
