c5cc67b224e34e5c2e5c54a2a4fbc0533ec68dcf
Mateusz Radwański <mateuszradwanski@icloud.com>
2022-03-08 19:45:32 +0100

    Dependency Injection for Application layer

---
+++ src/Negotiations.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Negotiations.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        
+        return services;
+    }
+}
